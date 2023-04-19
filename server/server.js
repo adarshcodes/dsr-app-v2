96b745dce89fb6f8ef421714f1429d26f2a856b9
Nikhil Vishwakarma <catalyst16812@gmail.com>
2023-04-19 12:05:03 +0530

    changes by nikhil

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Router = require("./routes")
+// const model = require("./model")
+// const jsonData = require('./data.json');
+
+// create the Express app
+const app = express();
+app.use(express.json());
+
+//catalyst16812
+
+// connect to the MongoDB database using Mongoose
+mongoose.connect('mongodb+srv://catalyst16812:nikhil11111@origindb.ginrwdp.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error(err));
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
+
+
+  // model.insertMany(jsonData)
+  // .then(function(docs) {
+  //   console.log(docs.length + ' documents inserted');
+  //   mongoose.disconnect();
+  // })
+  // .catch(function(err) {
+  //   console.error(err);
+  //   mongoose.disconnect();
+  // });
+
+// start the server
+app.use(Router);
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
