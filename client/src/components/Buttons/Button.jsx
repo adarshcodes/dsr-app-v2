684c92c935a9107db01a1170e084282391a60d5e
ayush-30-2k <80163767+ayush-30-2k@users.noreply.github.com>
2023-04-20 12:24:24 +0530

    added props in the button

--- client/src/components/Buttons/Button.jsx
+++ client/src/components/Buttons/Button.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-function Button({ value, varient, customClass, icon }) {
-	return (
-		<div
-			className={`btn ${
-				varient === "btn-light" ? "btn-light" : "btn-dark"
-			} ${customClass}`}
-		>
-			{icon && <img src={icon} alt="ms icon" />}
+function Button({ value, varient, customClass, icon, onclick }) {
+  return (
+    <div
+      className={`btn ${
+        varient === "btn-light" ? "btn-light" : "btn-dark"
+      } ${customClass}`}
+      onClick={(e) => onclick()}
+    >
+      {icon && <img src={icon} alt="ms icon" />}
 
-			<p>{value}</p>
-		</div>
-	);
+      <p>{value}</p>
+    </div>
+  );
 }
 
 export default Button;
