d1714371ab2aa055899b68eac39a2a886be8939a
samarthsharma17 <111560424+samarthsharma17@users.noreply.github.com>
2023-04-21 11:57:31 +0530

    organized files

---
+++ server/routes/user_route.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const userModel = require("../models/usermodel");
+
+const app = express();
+
+//*********************user calls***********************
+
+
+//create a user
+
+
+app.post("/add_user", async (request, response) => {
+    const user = new userModel(request.body);
+  
+    try {
+      await user.save();
+      response.send(user);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
+
+//retrieve all user
+app.get("/users", async (request, response) => {
+    const users = await userModel.find({});
+  
+    try {
+      response.send(users);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+
+  module.exports = app;
---
+++ server/routes/web_route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/new.html");
+})
+
+module.exports = app;
