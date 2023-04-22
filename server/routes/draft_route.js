7ea22a8f397266d62a3fd53a26c60a2877ecd45f
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2023-04-22 12:37:36 +0530

    fixing conflict issues

---
+++ server/routes/draft_route.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const draftModel = require("../models/draftmodel")
+const userModel = require("../models/usermodel")
+
+const app = express();
+
+//***************************Draft-calls*******************************8 */
+    //save a Draft record related to a user
+    app.post("/add_draft/", async (request, response) => {
+        const user = request.body.user;
+        const uservalid = await userModel.findById(user);
+      
+        if (!uservalid) {
+          return response.status(404).send("User not found"+user);
+        }
+      
+        const draft = new draftModel({
+          ...request.body
+        });
+      
+        try {
+          await draft.save();
+          response.send(draft);
+        } catch (error) {
+          response.status(500).send(error);
+        }
+      });
+    
+      //retrieve the draft records of a user
+      app.post("/users/draft", async (request, response) => {
+        const userId = request.body.user;
+      
+        try {
+          const draft = await draftModel.find({ user: userId });
+          response.send(draft);
+        } catch (error) {
+          response.status(500).send(error);
+        }
+      });
+  
+      //delete the draft record
+      app.delete("/draftdelete", async (request, response) => {
+        const draftId = request.body.draft;
+      
+        try {
+          const draft = await draftModel.findByIdAndDelete(draftId);
+          if (!draft) {
+            return response.status(404).send("Draft not found");
+          }
+          response.send("Draft deleted successfully");
+        } catch (error) {
+          response.status(500).send(error);
+        }
+      });
+      
+    module.exports = app;
---
+++ server/routes/user_route.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const userModel = require("../models/usermodel");
+const { ObjectId } = require('mongodb');
+const app = express();
+
+//*********************user calls***********************
+
+
+//create a user
+app.post("/add_user", async (request, response) => {
+    const user = new userModel(request.body);
+  
+    try {
+      await user.save();
+      response.send(user);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
+
+//retrieve all user
+app.get("/users", async (request, response) => {
+    const users = await userModel.find({});
+  
+    try {
+      response.send(users);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+
+  
+
+  //retrieve one user
+app.post("/finduser", async (request, response) => {
+  const userId = request.body.user;
+  
+  try {
+    const user = await userModel.findOne({ _id: userId });
+
+    if (!user) {
+      return response.status(404).send("user not found");
+    }
+    response.send(user);
+    // res.json(entity);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+module.exports = app;
