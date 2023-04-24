5a629354c2d010558c131d644fae71d3b7c06698
ayush-30-2k <80163767+ayush-30-2k@users.noreply.github.com>
2023-04-24 10:57:38 +0530

    testing auth

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0Provider
+        domain="dev-q5oc5s1eezm6wb5g.us.auth0.com"
+        clientId="JPP1i56C3PL59kFd9fGtASqtyt3TiKDL"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- client/src/routes/Login/Login.jsx
+++ client/src/routes/Login/Login.jsx
@@ -1,41 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import AnimatedComponent from "../../AnimatedComponent";
 import Logo from "../../assets/images/logo/logo-leaf.svg";
 import Icon from "../../assets/images/logo/ms.svg";
 import Quadrafort from "../../assets/images/logo/quadrafort-dark.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-	return (
-		// Adding animated component to make the route change animated -- Adarsh(19-Apr)
-		<AnimatedComponent>
-			<div className="login-container">
-				<div className="login-card">
-					<div className="top-part">
-						<img src={Logo} alt="logo" className="logo" />
+  const { loginWithPopup } = useAuth0();
 
-						<h1 className="heading-s">LeafLog</h1>
-						<p className="para">Stay connected to your Work and Nature</p>
-					</div>
+  return (
+    // Adding animated component to make the route change animated -- Adarsh(19-Apr)
+    <AnimatedComponent>
+      <div className="login-container">
+        <div className="login-card">
+          <div className="top-part">
+            <img src={Logo} alt="logo" className="logo" />
 
-					<form className="form login-form">
-						<Link to="/">
-							<div className="btn btn-light btn-light-shadow">
-								<img src={Icon} alt="ms-login" />
-								<p>Sign in with Microsoft</p>
-							</div>
-						</Link>
-					</form>
+            <h1 className="heading-s">LeafLog</h1>
+            <p className="para">Stay connected to your Work and Nature</p>
+          </div>
 
-					<div className="branding">
-						Proudly by
-						<img src={Quadrafort} alt="Quadrafort" className="quadra-logo" />
-					</div>
-				</div>
-			</div>
-		</AnimatedComponent>
-	);
+          <form className="form login-form">
+            {/* <Link to="/"> */}
+            <div
+              className="btn btn-light btn-light-shadow"
+              onClick={() => loginWithPopup()}
+            >
+              <img src={Icon} alt="ms-login" />
+              <p>Sign in with Microsoft</p>
+            </div>
+            {/* </Link> */}
+          </form>
+
+          <div className="branding">
+            Proudly by
+            <img src={Quadrafort} alt="Quadrafort" className="quadra-logo" />
+          </div>
+        </div>
+      </div>
+    </AnimatedComponent>
+  );
 }
 
 export default Login;
