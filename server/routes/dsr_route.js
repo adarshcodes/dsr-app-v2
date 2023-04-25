f4299530b49ee1261bc221efa95a1374d8592458
samarthsharma17 <111560424+samarthsharma17@users.noreply.github.com>
2023-04-25 17:29:49 +0530

    return statements

--- server/routes/dsr_route.js
+++ server/routes/dsr_route.js
@@ -4,21 +4,18 @@ const userModel = require("../models/usermodel");
 
 const app = express();
 
-
-function getist(){
-  
+function getist() {
   var currentTime = new Date();
   var currentOffSet = currentTime.getTimezoneOffset();
   var ISTOffSet = 330;
-  var ISTTime = new Date(currentTime.getTime() + (ISTOffSet + currentOffSet)*60000);
+  var ISTTime = new Date(
+    currentTime.getTime() + (ISTOffSet + currentOffSet) * 60000
+  );
   return ISTTime;
 }
 
 //global.userString = new String("User not found");
 //********************dsr calls************************
-
-
-
 
 //retrive all dsr
 app.get("/dsr", async (request, response) => {
@@ -192,16 +189,12 @@ app.post("/lastdsr", async (request, response) => {
   }
 });
 
-
-
-
 //edit dsr
 app.post("/saveupdate", async (request, response) => {
   const dsr = request.body._id;
   const dsrvalid = await dsrModel.findById(dsr);
 
-
-  let updatetime = new Date() ;
+  let updatetime = new Date();
   if (!dsrvalid) {
     return response.status(705).send();
   }
@@ -227,10 +220,6 @@ app.post("/saveupdate", async (request, response) => {
   }
 });
 
-
-
-
-
 //to check whether the user is on leave,dsr has not been filled, user has already saved dsr
 app.post("/todaystatus", async (request, response) => {
   const user = request.body.user;
@@ -239,27 +228,26 @@ app.post("/todaystatus", async (request, response) => {
     return response.status(702).send();
   }
   try {
-    if(!uservalid.lastdsrtime){
+    if (!uservalid.lastdsrtime) {
       return response.status(707).send("0");
     }
     let lastdate = new Date(uservalid.lastdsrtime);
-    let today = new Date()
-    if (lastdate.getDate() != today.getDate()){
+    let today = new Date();
+    if (lastdate.getDate() != today.getDate()) {
       //dsr not filled
-      response.send("0");
-    }
-    else {
+      return response.send("0");
+    } else {
       const dsr = await dsrModel.findOne({ user: user }).sort({ _id: -1 });
-      if(!dsr){
-        response.send("3");
+      if (!dsr) {
+        return response.send("3");
       }
-      if(dsr.isOnLeave){
+      if (dsr.isOnLeave) {
         //user was on leave
-      response.send("2");}
-      else{
+        return response.send("2");
+      } else {
         //user has saved dsr
-      response.send("1");}
-
+        response.send("1");
+      }
     }
   } catch (error) {
     response.status(500).send(error);
